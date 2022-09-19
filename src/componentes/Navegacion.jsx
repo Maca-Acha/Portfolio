78f24410231c32713fe6d40d2583dd923bf23db3
Maca-Acha <achamariamacarena@gmail.com>
2022-09-19 15:34:28 -0300

    nav

---
+++ src/componentes/Navegacion.jsx
@@ -0,0 +1,17 @@
+export default function Navegacion() {
+
+    return (
+        <div className="nav-completo">
+            <div className="nav2">
+                <a className="link-nav">Inicio</a>
+            </div>
+            <div className='nav'>
+                <a className="link-nav">Sobre mi</a>
+                <a className="link-nav">Proyectos</a>
+                <a className="link-nav">Habilidades</a>
+                <a className="link-nav">Contacto</a>
+            </div>
+        </div>
+
+    )
+}
