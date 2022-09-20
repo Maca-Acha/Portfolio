be8f62cad06999105cbf2d6b5af54b9bd511d550
Maca-Acha <achamariamacarena@gmail.com>
2022-09-20 09:35:35 -0300

    componente-inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navigation from './componentes/Navegacion'
+import Inicio from './componentes/inicio'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,10 +9,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Navigation/>
-      
-    </div>
+    
+      <div className="App">
+        <Navigation/>
+        <Inicio/>
+      </div>
+
   )
 }
 
---
+++ src/componentes/inicio.jsx
@@ -0,0 +1,19 @@
+import foto from "../assets/foto-principal.png"
+
+export default function inicio() {
+    return (
+        <div className="inicio">
+            <div className="inicio-texto">
+                <div className="primera-linea">
+                    <h1>Hola, soy</h1>
+                    <h1 className="nombre"> Macarena Acha</h1>
+                </div>
+                <h1>Desarrolladora Full Stack
+                </h1>
+            </div>
+            <div className="contenedor-foto">
+                <img alt="foto" src={foto} className="foto" />
+            </div>
+        </div>
+    )
+}
