9ad616e89eb80e043564fdf3a9dda4d6a05a68e5
Maca-Acha <achamariamacarena@gmail.com>
2022-10-27 20:14:43 -0300

    integracion contacto con back

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import Contacto from './pages/Contacto'
 import {Parallax} from 'react-parallax'
 import fondo1 from './assets/img1.svg'
 import fondo2 from './assets/img2.svg'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
       <div className='App' >
+        <ToastContainer />
         <Parallax bgImage={fondo1} strength={400} bgClassName="fondo-parallax">
           <Navigation/>
           <Inicio />
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -4,7 +4,31 @@ import 'aos/dist/aos.css'
 import { useEffect } from 'react';
 import { Parallax } from 'react-parallax';
 import fondo from '../assets/img4.svg'
+import axios from "axios"
+import {toast } from 'react-toastify';
 
+
+const handleSubmit = async (e)=> {
+    e.preventDefault()
+    const data = {
+        nombre: e.target.nombre.value,
+        asunto: e.target.asunto.value,
+        email: e.target.email.value,
+        mensaje: e.target.mensaje.value,
+    }
+    try{
+        if(data.nombre && data.asunto && data.email && data.mensaje){
+            const response = await axios.post(`${import.meta.env.VITE_SOME_KEY}contacts`,data)
+            toast.success(response.data.response)
+            e.target.reset()
+        }else{
+            toast.error("Faltan campos por completar")
+        }
+    }catch(error){
+        toast.error(error.message)
+
+    }
+}
 
 export default function Contacto() {
     useEffect(()=>{
@@ -21,12 +45,12 @@ export default function Contacto() {
                         <div className="cont-texto-contacto" data-aos="fade-right">
                             <p>Busco la oportunidad de insertarme laboralmente dispuesta a capacitarme para mejorar mis capacidades, e integrarme en el mundo digital que es lo que me apasiona. </p>
                         </div>
-                        <form className="formulario" data-aos="fade-left">
+                        <form className="formulario" data-aos="fade-left" onSubmit={handleSubmit}>
                             <div className="inputs">
                                 <input type="text"  required minLength="3" maxLength="25" className="input input1" name="nombre" placeholder="Nombre completo"/>
-                                <input type="text"  className="input" name="email"  required maxLength="25" placeholder="Email" />
+                                <input type="email"  className="input" name="email"  required maxLength="25" placeholder="Email" />
                                 <input type="text" className="input" name="asunto" required placeholder="Asunto"/>
-                                <textarea className="input input-area" name="texto" placeholder="Tu mensaje"/>
+                                <textarea className="input input-area" name="mensaje" placeholder="Tu mensaje"/>
                             <div className='submit'>
                                 <input type="submit" value="Enviar" className='btn-submit'  />
                             </div>
