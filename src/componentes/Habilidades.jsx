b24574244b67f758096953a65e9078fa86bba6b0
Maca-Acha <achamariamacarena@gmail.com>
2022-10-28 20:36:18 -0300

    ajustes de logos de habilidades

--- src/componentes/Habilidades.jsx
+++ src/componentes/Habilidades.jsx
@@ -2,7 +2,7 @@ import html from "../assets/html.png"
 import css from "../assets/css.png"
 import js from "../assets/javascript.png"
 import node from "../assets/node.png"
-import heroku from "../assets/heroku.png"
+import netlify from "../assets/logomark.svg"
 import react from "../assets/react.svg"
 import vite from "../assets/vite.png"
 import postman from "../assets/postman.png"
@@ -12,6 +12,7 @@ import git from "../assets/git.png"
 import bootstrap from "../assets/bootstrap.png"
 import express from "../assets/express.png"
 import mongo from "../assets/mongo.png"
+import firebase from "../assets/firebase.png"
 import AOS from 'aos';
 import 'aos/dist/aos.css'
 import { useEffect } from 'react';
@@ -22,13 +23,14 @@ const logos = [
     {nombre:"CSS", src: css, id:"css-logo", i: 1},
     {nombre:"Java Script", src: js, id:"js-logo", i: 2},
     {nombre:"Node", src: node, id:"node-logo", i: 3},
-    {nombre:"Heroku", src: heroku, id:"heroku-logo", i: 4}
+    {nombre:"Netlify", src: netlify, id:"netlify-logo", i: 4}
 ]
 const logos2 = [
     {nombre:"React", src: react, id:"react-logo", i: 5},
     {nombre:"Vite", src: vite, id:"vite-logo", i: 6},
     {nombre:"Postman", src: postman, id:"postman-logo", i: 7},
     {nombre:"Redux", src: redux, id:"redux-logo", i: 8},
+    {nombre:"Firebase", src: firebase, id:"firebase-logo", i: 9}
 ]
 const logos3 = [
     {nombre:"Github", src: github, id:"github-logo", i: 9},
